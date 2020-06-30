f8d4af02017d6a424b3458be5d0398af9be71adc
Matt Clark <MATTHEWCLARK989@boisestate.edu>
2020-06-30 16:15:56 -0600

    can't parallelize using mc.cores because windows doesn't allow parallelization in this way.

--- Abm_MultipleReplicationsPlots.R
+++ Abm_MultipleReplicationsPlots.R
@@ -270,7 +270,7 @@ abm<-function(#Specified parameters
   
 }
 
-abm(Runs=3,CoopPercStart = 0.8, ProbOfMobility=0.45 )
+abm(Runs=10,CoopPercStart = 0.8, ProbOfMobility=0.45 )
 
 #fill a dataframe with the means
 
@@ -287,6 +287,10 @@ FillData[i,3]<-apply(FullOutput$percCCProtect[75,1:10],1,mean)
 }
   
 
-#try with parallel
-library(parellel)
+
+
+
+
+
+
 
