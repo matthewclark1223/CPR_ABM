8f892ab69f15aa792e45a2011820f58a33519ba7
Matt Clark <Matthewclark989@boisestate.edu>
2021-05-26 13:47:45 -0600

    tree cover abm, still need to add in two different deforestation rates

---
+++ PembaTreeCover.R
@@ -0,0 +1,96 @@
+library("raster")
+library("rgeos")
+library("rgdal")
+library(sf)
+library(magrittr)
+#library("rasterVis")
+#library(viridis)
+
+
+x<-raster("Hansen_GFC-2019-v1.7_treecover2000_00N_030E.tif") #tree cover data from https://data.globalforestwatch.org/datasets/tree-cover-2000
+#plot(x) this takes awhile to run and is worthless for this demonstration besides making sure it worked
+
+Tanzania<-read_sf("~/Pemba_Project/MapData/TZA_adm0.shp") #tanzania jurisdictional boundaries
+Pemba <- st_crop(Tanzania, xmin = 39.35, xmax = 40.0,
+                 ymin = -5.75, ymax = -4.85)  #pemba only
+
+z<-crop(x,Pemba)#crop to pemba
+z[z==0]<-NA #make water NA
+plot(z) #make sure this worked for the initial map
+
+
+model <- function(r, Time) { #this is the very simple model 
+  outputs <- vector("list", Time) #list of length 'time'
+  outputs[[1]]<-r #the first object in the list is the initialization raster
+  for(t in 2:Time) { 
+    
+    r_old<-outputs[[t-1]] #raster from last time step is now the thing getting things done to it
+     new_cov<-r_old-(0.1*r_old) # minus a deforestation rate of 3%
+     new_cov[z<0]<-0 #don't let % cover go below 0
+     outputs[[t]] <-new_cov
+      
+  }
+  return(outputs) #return rasters as list
+}
+
+Mod <- model(r = z, Time = 5) #run the model, create 5 rasters
+
+
+### 3 #################################################
+####plot the output
+
+#plot(stack(dumMod) ) #this is a simpler, but worse option that the code below
+
+levelplot(stack(Mod) )  #make a raster stack and plot
+
+source("MakePembaVector.R") #load the vector data with the deforestation rates from Collins et al 2021
+
+#make a raster with the values for deforestation for each pixel so we can multiply times the 
+#2000 tree cover
+
+def2001_rast<-raster()
+#extent(def2001_rast)<-extent(Pemba_vector)  dont run
+extent(def2001_rast)<-extent(z) 
+  
+res(def2001_rast)<-res(z) #set the pixel size to be the same
+
+Pem_M<-st_cast(Pemba_vector,"MULTIPOLYGON")
+def1<-fasterize::fasterize(Pem_M,def2001_rast,field="Annual_rate_of_change_2001_2010" )
+def2<-fasterize::fasterize(Pem_M,def2001_rast,field="Annual_rate_of_change_2010_2018" )
+
+plot(def2)
+
+vec_rast<-list(def1,def2)
+plot(stack(vec_rast))
+
+plot(def2)
+plot(z)
+
+plot(z*(def2*0.01))
+
+#now run the look
+model <- function(r, Time) { #this is the very simple model 
+  outputs <- vector("list", Time) #list of length 'time'
+  outputs[[1]]<-r #the first object in the list is the initialization raster
+  for(t in 2:Time) { 
+    
+    r_old<-outputs[[t-1]] #raster from last time step is now the thing getting things done to it
+    new_cov<-r_old-(r_old*(def2*0.01)) # 
+    new_cov[z<0]<-0 #don't let % cover go below 0
+    outputs[[t]] <-new_cov
+    
+  }
+  return(outputs) #return rasters as list
+}
+
+Mod <- model(r = z, Time = 5) #run the model, create 5 rasters
+
+
+### 3 #################################################
+####plot the output
+
+#plot(stack(dumMod) ) #this is a simpler, but worse option that the code below
+
+levelplot(stack(Mod) )  #make a raster stack and plot
+da<-stack(Mod)
+animate(da)
--- RasterPlot.R
+++ RasterPlot.R
@@ -21,27 +21,100 @@ Resrc<-rbinom(Resources,1,StartingIntegrity)
 
 # Visualize results
 
-DegRaster <- rasterFromXYZ(cbind(simgrid[, 1:2] - 0.5, Resrc))
+#DegRaster <- rasterFromXYZ(cbind(simgrid[, 1:2] - 0.5, Resrc))
 
-plot(DegRaster)
+#plot(DegRaster)
 
 #create protected area raster
-NumProtec<-Resources*PercProtec
-Protected <-c(rep(2,NumProtec),rep(1,Resources-NumProtec)) #Dummy values for protected vs not
-ProtDat<-cbind(simgrid[, 1:2] - 0.5,Protected)
-ProtRaster<-rasterFromXYZ(ProtDat)#make the categorical raster
-plot(ProtRaster,
-     legend = FALSE,
-     col = c("blue", "green"), axes = FALSE,
-     main = "words")
+#NumProtec<-Resources*PercProtec
+#Protected <-c(rep("Protected",NumProtec),rep("Not Protected",Resources-NumProtec)) #Dummy values for protected vs not
+#ProtDat<-cbind(simgrid[, 1:2] - 0.5,Protected)
+#ProtRaster<-rasterFromXYZ(ProtDat)#make the categorical raster
+#plot(ProtRaster,
+     #legend = FALSE,
+     #col = c("blue", "green"), axes = FALSE,
+     #main = "words")
 
-legend("topright",
-       legend = c("Not Protected", "Protected"),
-       fill = c( "blue", "green"),
-       border = FALSE,
-       bty = "n") # turn off legend border
+#legend("topright",
+      # legend = c("Not Protected", "Protected"),
+      # fill = c( "blue", "green"),
+      # border = FALSE,
+      # bty = "n") # turn off legend border
 
 #Make the raster stack
 
-dat<-stack(DegRaster,ProtRaster)
-plot(dat)
+#dat<-stack(DegRaster,ProtRaster)
+#plot(dat)
+
+
+
+##gt paper plot
+Resources<-10000
+simgrid <- expand.grid(1:sqrt(Resources), 1:sqrt(Resources))
+n <- nrow(simgrid)
+
+
+Protected <-c(rep(2,NumProtec),rep(0,Resources-NumProtec))
+PercProtec<-0.0
+NumProtec<-Resources*PercProtec
+StartingIntegrity<-1
+Resrc<-rbinom(Resources,1,StartingIntegrity)
+Protected <-c(rep("Protected",NumProtec),rep("Working",Resources-NumProtec)) #Dummy values for protected vs not
+ProtDat<-cbind(simgrid[, 1:2] - 0.5,Protected)
+x<-cbind(ProtDat,Resrc)
+mp1<-ggplot() +
+  geom_raster(data = x , aes(x = Var1, y = Var2, fill = Protected)) + theme_void()+
+  coord_quickmap()+
+  scale_fill_manual(name="Area",breaks=c("Working"),values=c("#1f78b4"))
+
+
+
+Protected <-c(rep(2,NumProtec),rep(0,Resources-NumProtec))
+PercProtec<-0.6
+NumProtec<-Resources*PercProtec
+StartingIntegrity<-1
+Resrc<-rbinom(Resources,1,StartingIntegrity)
+Protected <-c(rep("Protected",NumProtec),rep("Working",Resources-NumProtec)) #Dummy values for protected vs not
+ProtDat<-cbind(simgrid[, 1:2] - 0.5,Protected)
+x<-cbind(ProtDat,Resrc)
+
+mp2<-ggplot() +
+  geom_raster(data = x , aes(x = Var1, y = Var2, fill = Protected)) + theme_void()+
+  coord_quickmap()+
+  scale_fill_manual(name="Area",breaks=c("Working","Protected",""),values=c("#33a02c","#1f78b4","white"))
+
+
+Protected <-c(rep(2,NumProtec),rep(0,Resources-NumProtec))
+PercProtec<-0.6
+NumProtec<-Resources*PercProtec
+StartingIntegrity<-0.2
+Resrc<-rbinom(Resources,1,StartingIntegrity)
+Protected <-c(rep("Protected",NumProtec),rep("Working",Resources-NumProtec)) #Dummy values for protected vs not
+ProtDat<-cbind(simgrid[, 1:2] - 0.5,Protected)
+x<-cbind(ProtDat,Resrc)
+x[x$Resrc==0,]$Protected<-NA
+
+mp3<-ggplot() +
+  geom_raster(data = x , aes(x = Var1, y = Var2, fill = Protected)) + theme_void()+
+  coord_quickmap()+
+  scale_fill_manual(name="Area",breaks=c("Working","Protected",""),values=c("#33a02c","#1f78b4","white"))
+
+Protected <-c(rep(2,NumProtec),rep(0,Resources-NumProtec))
+PercProtec<-0.2
+NumProtec<-Resources*PercProtec
+StartingIntegrity<-0.2
+Resrc<-rbinom(Resources,1,StartingIntegrity)
+Protected <-c(rep("Protected",NumProtec),rep("Working",Resources-NumProtec)) #Dummy values for protected vs not
+ProtDat<-cbind(simgrid[, 1:2] - 0.5,Protected)
+x<-cbind(ProtDat,Resrc)
+x[x$Resrc==0,]$Protected<-NA
+
+mp4<-ggplot() +
+  geom_raster(data = x , aes(x = Var1, y = Var2, fill = Protected)) + theme_void()+
+  coord_quickmap()+
+  scale_fill_manual(name="Area",breaks=c("Working","Protected",""),values=c("#33a02c","#1f78b4","white"))
+
+
+
+
+
